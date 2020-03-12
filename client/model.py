8e2aed27a890f8c1fc39a5551fb52a891307dea3
ninoo096 <ninooo96@gmail.com>
2020-03-12 12:46:05 +0100

    Implemented some connection utility

---
+++ client/model.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("C:\\Users\\ninoo\\Documents\\Python project\\Graphic Tablet\\")
+import connection as conn
+utility = conn.connectionUtility
+connection = conn.connection
+
+
+utility.searchDevices()
--- connection.py
+++ connection.py
@@ -3,9 +3,48 @@
 
  #https://people.csail.mit.edu/albert/bluez-intro/x232.html
 import bluetooth
-class Connection:
+import sys
+class connection:
+    """ Class to establish a bluetooth connection Client-Server """
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.socket = socket
 
-    def send(host, port):
+    def send(self, message):
+        self.socket.send(message)
+    
+    def connect(self):
+        self.socket.connect((self.host, self.port))
+
+    def close(self):
+        self.socket.close()
+    
+    #Server Methods. Port must be choose by Server app
+    def listen(self):
+        self.socket.bind(("", self.port))
+        self.socket.listen(1) #accept 1 connection at a time
+    
+    def accept(self):
+        client_socket, address = self.socket.accept()
+        return client_socket, address
+    
+    def receive(self, client_socket):
+        data = client_socket.recv(1024)
+        print("received", data)
+        return data
+    
+class connectionUtility:
+    """ Utility methods for Bluetooth Connection """
+    @classmethod
+    def searchDevices(cls):
+        print ("Search devices...")
+        nearby_devices = bluetooth.discover_devices(lookup_names = True)
+        print ("Found %d devices" % len(nearby_devices))
+
+        for name, addr in nearby_devices:
+            print (" %s - %s" % (addr, name))
+        
+if __name__ == "__main__":
+    connectionUtility.searchDevices()
